565102a3d9f5403e2b6fd1af742b6797279c3380
arman-b <47083206+arman-b@users.noreply.github.com>
2021-05-31 17:32:45 -0400

    Update forkfile.h

--- forkfile.h
+++ forkfile.h
@@ -1,13 +1,5 @@
 #if !defined(FORKFILE_H)
 #define FORKFILE_H
-
-/* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
-
-/* DO NOT MODIFY THIS FILE OR YOUR CODE WILL NOT COMPILE ON THE SUBMIT
-   SERVER */
-
 #include "forkfile-datastructure.h"
 
 Forkfile read_forkfile(const char filename[]);
