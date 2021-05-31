9766caf1e27e036aeb7b5b25ae2090da122e58ba
arman-b <47083206+arman-b@users.noreply.github.com>
2021-05-31 17:34:40 -0400

    Update make-target.h

--- make-target.h
+++ make-target.h
@@ -1,12 +1,6 @@
 #if !defined(MAKE_TARGET_H)
 #define MAKE_TARGET_H
-
-/* just documenting that this header file uses definitions in forkfile.h */
 #include "forkfile.h"
-
-/* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
 
 int make_target(Forkfile forkfile, const char target_name[]);
 
