62571546c82fdc03a175ff3d81ad3f622587b1b3
arman-b <47083206+arman-b@users.noreply.github.com>
2021-05-31 17:33:43 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
 
-/* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
-
+/* Example */
 int main(void) {
   int a= 112, b= 104;
 
